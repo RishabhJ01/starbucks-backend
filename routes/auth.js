549f61eadd5f92c247a61157bf379868ccb91b40
Reeshabh Jain <jain2001.rishabh@gmail.com>
2023-07-28 23:21:06 +0530

    feat: :tada: Initialized the project using typescript

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const {registerUser, loginUser} = require('../controllers/authController.js')
-
-router.post("/signup",registerUser);
-
-router.post("/login", loginUser);
-
-module.exports = router;
