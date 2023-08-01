631c93a053e3481c8b0f3c4d4aa712591272b5de
Reeshabh Jain <jain2001.rishabh@gmail.com>
2023-08-01 19:16:32 +0530

    created dockerfile

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 const app = express();
 const port = 3000;
+const host = "0.0.0.0";
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
