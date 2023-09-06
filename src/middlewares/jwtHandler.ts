eaf604e8e414b7508b5a2e65bef690f01106fa4a
Reeshabh Jain <jain2001.rishabh@gmail.com>
2023-09-06 22:41:53 +0530

    fix: :bug: maintaining this branch as the main backend for frontend

--- src/middlewares/jwtHandler.ts
+++ src/middlewares/jwtHandler.ts
@@ -1,2 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
+import { User,IUser } from "../models/users";
 import * as jwt from 'jsonwebtoken';
+
+export const handleJwt = async (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const token = req.cookies['access-token'];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) as jwt.JwtPayload;
+        const data:any = await User.find({email: decoded.email});
+        
+        if(data.length === 0){
+            return res.status(401).json({message: "Unauthorized!"});
+        }
+        const user:IUser = data[0]
+        if((user.first_name !== decoded.first_name) || (user.last_name !== decoded.last_name) || (user.phone !== decoded.phone)){
+            return res.status(401).json({message: "Unauthorized"});
+        }
+        res.locals.user = user;
+        next();
+    }catch(err){
+        return res.status(500).json({message: "Internal server error!"});
+    }
+    
+}
